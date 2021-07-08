f71c2ad4b11e6b7ed0e9e4fe6cdce9bd296e47f6
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-07-09 02:00:19 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/fazrul1994/Braindrill-TCbuild $(pwd)/llvm-tc -b llvm-tc_template
+git clone https://github.com/fazrul1994/Braindrill-TCbuild $(pwd)/llvm-tc
 cd $(pwd)/llvm-tc
 bash build-tc.sh
